14f48acc8ec017a6343fff9f6f351f12e624c79b
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-07-09 16:05:28 +0530

    Added command in vorpal for checking the status of any job id.

--- interface/app.js
+++ interface/app.js
@@ -1,15 +1,15 @@
 const http = require('http');
 const vorpal = require('vorpal')();
 
-const getServerOptions = (src, target, level) => ({
+const getServerOptions = (method) => ({
   host: 'localhost',
   port: 8080,
-  method: 'POST',
-  path: `/findConnection/${src}/${target}/${level}`,
+  method,
 });
 
-const sendRequest = (src, target, level) => {
-  const options = getServerOptions(src, target, level);
+const sendRequest = (path, method) => {
+  const options = getServerOptions(method);
+  options.path = path;
   return new Promise((resolve, reject) => {
     const req = http.request(options, (res) => {
       let data = '';
@@ -29,8 +29,22 @@ vorpal
   .alias('sp')
   .action((args, cb) => {
     const { src, target, level } = args;
-    sendRequest(src, target, level).then((res) => {
+    const path = `/findConnection/${src}/${target}/${level}`;
+    sendRequest(path, 'POST').then((res) => {
       console.log(res);
+      cb();
+    });
+  });
+
+vorpal
+  .command('status <jobId>')
+  .description('for checking the current status of a job')
+  .autocomplete(['jobId'])
+  .action((args, cb) => {
+    const { jobId } = args;
+    const path = `/status/${jobId}`;
+    sendRequest(path, 'GET').then((res) => {
+      console.table(JSON.parse(res));
       cb();
     });
   });
